aa2e4575dc4ae47b46c5c2bc71f86fa5297f492b
STRlantian <M1606977882@outlook.com>
2022-11-27 15:02:17 +0800

    2022.11.27 Project Start

--- files/study/scripts/Password.js
+++ files/study/scripts/Password.js
@@ -1,6 +1,15 @@
 var password = "cloudclass2"
+var login = false
 
 function main()
 {
     window.alert("Personal Page, please input password")
+}
+
+function check()
+{
+    if(!login)
+    {
+        
+    }
 }
